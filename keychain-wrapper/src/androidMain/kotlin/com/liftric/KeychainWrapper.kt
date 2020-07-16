e4894b03a2fc4584a7aa9a5de90e001024300f14
Ben John <john@liftric.com>
2020-07-16 12:30:27 +0200

    refactor(KeychainWrapper): remove java doc from actual implementation

--- keychain-wrapper/src/androidMain/kotlin/com/liftric/KeychainWrapper.kt
+++ keychain-wrapper/src/androidMain/kotlin/com/liftric/KeychainWrapper.kt
@@ -19,11 +19,6 @@ actual class KeychainWrapper(private val context: Context) {
         )
     }
 
-    /**
-     * Saves a string value in the keychain.
-     * @param value The value to store
-     * @param forKey The key to query
-     */
     actual fun set(value: String, forKey: String): Boolean {
         return encSharedPrefs
             .edit()
@@ -31,11 +26,6 @@ actual class KeychainWrapper(private val context: Context) {
             .commit()
     }
 
-    /**
-     * Saves an int value in the keychain.
-     * @param value The value to store
-     * @param forKey The key to query
-     */
     actual fun set(value: Int, forKey: String): Boolean {
         return encSharedPrefs
             .edit()
@@ -43,11 +33,6 @@ actual class KeychainWrapper(private val context: Context) {
             .commit()
     }
 
-    /**
-     * Saves a long value in the keychain.
-     * @param value The value to store
-     * @param forKey The key to query
-     */
     actual fun set(value: Long, forKey: String): Boolean {
         return encSharedPrefs
             .edit()
@@ -55,11 +40,6 @@ actual class KeychainWrapper(private val context: Context) {
             .commit()
     }
 
-    /**
-     * Saves a float value in the keychain.
-     * @param value The value to store
-     * @param forKey The key to query
-     */
     actual fun set(value: Float, forKey: String): Boolean {
         return encSharedPrefs
             .edit()
@@ -67,11 +47,6 @@ actual class KeychainWrapper(private val context: Context) {
             .commit()
     }
 
-    /**
-     * Saves a double value in the keychain.
-     * @param value The value to store
-     * @param forKey The key to query
-     */
     actual fun set(value: Double, forKey: String): Boolean {
         return encSharedPrefs
             .edit()
@@ -79,11 +54,6 @@ actual class KeychainWrapper(private val context: Context) {
             .commit()
     }
 
-    /**
-     * Saves a boolean value in the keychain.
-     * @param value The value to store
-     * @param forKey The key to query
-     */
     actual fun set(value: Boolean, forKey: String): Boolean {
         return encSharedPrefs
             .edit()
@@ -91,29 +61,14 @@ actual class KeychainWrapper(private val context: Context) {
             .commit()
     }
 
-    /**
-     * Checks if object with key exists in the keychain.
-     * @param forKey The key to query
-     * @return True or false, depending on wether it is in the keychain or not
-     */
     actual fun existsObject(forKey: String): Boolean {
        return encSharedPrefs.contains(forKey)
     }
 
-    /**
-     * Returns the string value of an object in the keychain.
-     * @param forKey The key to query
-     * @return The stored string value
-     */
     actual fun string(forKey: String): String? {
         return encSharedPrefs.getString(forKey, null)
     }
 
-    /**
-     * Returns the int value of an object in the keychain.
-     * @param forKey The key to query
-     * @return The stored int value
-     */
     actual fun int(forKey: String): Int? {
         if (existsObject(forKey)) {
             return encSharedPrefs.getInt(forKey, Int.MIN_VALUE)
@@ -121,11 +76,6 @@ actual class KeychainWrapper(private val context: Context) {
         return null
     }
 
-    /**
-     * Returns the long value of an object in the keychain.
-     * @param forKey The key to query
-     * @return The stored long value
-     */
     actual fun long(forKey: String): Long? {
         if (existsObject(forKey)) {
             return encSharedPrefs.getLong(forKey, Long.MIN_VALUE)
@@ -133,11 +83,6 @@ actual class KeychainWrapper(private val context: Context) {
         return null
     }
 
-    /**
-     * Returns the int value of an object in the keychain.
-     * @param forKey The key to query
-     * @return The stored double lue
-     */
     actual fun double(forKey: String): Double? {
         if (existsObject(forKey)) {
             return encSharedPrefs.getFloat(forKey, Float.MIN_VALUE).toDouble()
@@ -145,11 +90,6 @@ actual class KeychainWrapper(private val context: Context) {
         return null
     }
 
-    /**
-     * Returns the int value of an object in the keychain.
-     * @param forKey The key to query
-     * @return The stored float value
-     */
     actual fun float(forKey: String): Float? {
         if (existsObject(forKey)) {
             return encSharedPrefs.getFloat(forKey, Float.MIN_VALUE)
@@ -157,11 +97,6 @@ actual class KeychainWrapper(private val context: Context) {
         return null
     }
 
-    /**
-     * Returns the boolean value of an object in the keychain.
-     * @param forKey The key to query
-     * @return The stored boolean value
-     */
     actual fun bool(forKey: String): Boolean? {
         if (existsObject(forKey)) {
             return encSharedPrefs.getBoolean(forKey, false)
@@ -169,17 +104,10 @@ actual class KeychainWrapper(private val context: Context) {
         return null
     }
 
-    /**
-     * Deletes object with the given key from the keychain.
-     * @param forKey The key to query
-     */
     actual fun deleteObject(forKey: String): Boolean {
         return encSharedPrefs.edit().remove(forKey).commit()
     }
 
-    /**
-     * Deletes all objects with the service name from the keychain.
-     */
     actual fun clear() {
         encSharedPrefs.edit().clear().apply()
     }
