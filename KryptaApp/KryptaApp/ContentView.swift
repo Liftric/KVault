9f64ba0e8f340baa4fce9dc7d203d2606760d9ae
Ben John <john@liftric.com>
2020-07-16 17:46:26 +0200

    tests(ios): add skeleton for testing within host-app

---
+++ KryptaApp/KryptaApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KryptaApp
+//
+//  Created by Ben John on 16.07.20.
+//  Copyright © 2020 Liftric GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
