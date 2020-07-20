cc2223dc594e7bf3b0b13493dcf0cc651555f96e
Jan Gaebel <g.jan94@icloud.com>
2020-07-20 16:24:49 +0200

    feat(gradle): Add maven central and bintray publishing

--- kvault/build.gradle.kts
+++ kvault/build.gradle.kts
@@ -1,8 +1,30 @@
+import java.util.Date
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("maven-publish")
+    id("com.jfrog.bintray") version "1.8.4"
 }
+
+val artifactName = "KVault"
+val artifactGroup = project.group.toString()
+val artifactVersion = project.version.toString()
+
+val pomUrl = "https://github.com/Liftric/kvault"
+val pomScmUrl = "https://github.com/Liftric/kvault"
+val pomIssueUrl = "https://github.com/Liftric/kvault/issues"
+val pomDesc = "Secure key-value store for Kotlin Multiplatform projects"
+
+val githubRepo = "Liftric/kvault"
+val githubReadme = "README.md"
+
+val pomLicenseName = "MIT"
+val pomLicenseUrl = "https://github.com/Liftric/kvault/blob/LICENSE"
+val pomLicenseDist = "repo"
+
+val pomDeveloperId = "liftric"
+val pomDeveloperName = "Liftric"
 
 group = "com.liftric"
 version = "1.0"
@@ -75,5 +97,69 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("kvault") {
+            groupId = artifactGroup
+            artifactId = artifactName.toLowerCase()
+            version = artifactVersion
+
+            pom {
+                name.set(rootProject.name)
+                description.set(pomDesc)
+                url.set(pomUrl)
+
+                licenses {
+                    license {
+                        name.set(pomLicenseName)
+                        url.set(pomLicenseUrl)
+                    }
+                }
+                developers {
+                    developer {
+                        name.set(pomDeveloperName)
+                        id.set(pomDeveloperId)
+                    }
+                }
+                scm {
+                    connection.set(pomScmUrl)
+                    developerConnection.set(pomScmUrl)
+                    url.set(pomUrl)
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("BINTRAY_USER").toString()
+    key = project.findProperty("BINTRAY_PASSWORD").toString()
+    publish = false
+
+    setPublications("liftric")
+
+    pkg.apply {
+        repo = "maven"
+        name = artifactName
+        userOrg = pomDeveloperId
+        githubRepo = githubRepo
+        vcsUrl = pomScmUrl
+        description = pomDesc
+        setLabels("kotlin-multiplatform", "liftric", "kotlin-native", "keychain", "sharedpreferences", "key-value-store")
+        setLicenses(pomLicenseName)
+        desc = description
+        websiteUrl = pomUrl
+        issueTrackerUrl = pomIssueUrl
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = artifactVersion
+            desc = description
+            released = Date().toString()
+            vcsTag = artifactVersion
+        }
     }
 }
