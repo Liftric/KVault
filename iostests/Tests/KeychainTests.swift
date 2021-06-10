09dcab6163d35ff07028b07861717f8eb8328a33
Ben John <john@liftric.com>
2021-06-10 16:01:49 +0200

    fix: adjust assertion fail message

--- iostests/Tests/KeychainTests.swift
+++ iostests/Tests/KeychainTests.swift
@@ -235,7 +235,7 @@ class KeychainTests: XCTestCase {
         sut2.clear()
 
         keys.forEach {
-            XCTAssertTrue(sut.existsObject(forKey: $0), "\($0) should not exist anymore")
+            XCTAssertTrue(sut.existsObject(forKey: $0), "\($0) should still exist")
         }
 
         keys.forEach {
