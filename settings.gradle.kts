bf735bd0df70713760223fc21f9d4ac0d438687e
Jan Gaebel <gaebel.dev@icloud.com>
2022-11-07 15:27:28 +0700

    feat(project): bump to Kotlin 1.7.20 and remove buildSrc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,12 +6,25 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+}
 
-    resolutionStrategy {
-        eachPlugin {
-            if(requested.id.namespace == "com.android") {
-                useModule("com.android.tools.build:gradle:${requested.version}")
-            }
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            version("android-tools-gradle", "7.3.0")
+            version("kotlin", "1.7.20")
+            version("crypto", "1.1.0-alpha03")
+
+            library("security-crypto", "androidx.security", "security-crypto").versionRef("crypto")
+            library("androidx-test-core", "androidx.test", "core").version("1.2.0")
+            library("roboelectric", "org.robolectric", "robolectric").version("4.5.1")
+
+            plugin("versioning", "net.nemerosa.versioning").version("3.0.0")
         }
     }
 }
