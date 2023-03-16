a68887373e3c40ac2d5d02f9411dca792c86e693
Stanislav <dymedyuk@gmail.com>
2023-03-16 15:36:53 +0100

    Update Libs.kt
    Bumped androidx.security:security-crypto to 1.1.0-alpha05
    change log: https://developer.android.com/jetpack/androidx/releases/security?authuser=1#1.1.0-alpha05
    It should fix issue: https://developer.android.com/jetpack/androidx/releases/security?authuser=1#1.1.0-alpha05

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -9,7 +9,7 @@ object Versions {
     const val Gradle = "7.1.2"
     const val Kotlin = "1.6.20"
     const val Versioning = "2.15.1"
-    const val Crypto = "1.1.0-alpha03"
+    const val Crypto = "1.1.0-alpha05"
     const val RoboElectric = "4.5.1"
     const val TestCore = "1.4.0"
 }
