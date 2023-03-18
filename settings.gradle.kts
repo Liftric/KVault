af184f327e3d4400dd485d9c8b556157eb49ebc9
Ben John <john@liftric.com>
2023-03-18 09:17:20 +0100

    fix(deps): bumps androidx.security:security-crypto to 1.1.0-alpha05

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("android-tools-gradle", "7.3.0")
             version("kotlin", "1.7.20")
-            version("crypto", "1.1.0-alpha03")
+            version("crypto", "1.1.0-alpha05")
 
             library("security-crypto", "androidx.security", "security-crypto").versionRef("crypto")
             library("androidx-test-core", "androidx.test", "core").version("1.4.0")
