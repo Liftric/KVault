e2d2098d50eac23c1ce4fd7d8150c705de9d6d59
Ben John <john@liftric.com>
2023-03-18 09:45:20 +0100

    chore: bump deps and use iOS 16.2 for testing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,12 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("android-tools-gradle", "7.3.0")
-            version("kotlin", "1.7.20")
+            version("kotlin", "1.8.10")
             version("crypto", "1.1.0-alpha05")
 
             library("security-crypto", "androidx.security", "security-crypto").versionRef("crypto")
-            library("androidx-test-core", "androidx.test", "core").version("1.4.0")
-            library("roboelectric", "org.robolectric", "robolectric").version("4.9")
+            library("androidx-test-core", "androidx.test", "core").version("1.5.0")
+            library("roboelectric", "org.robolectric", "robolectric").version("4.9.2")
 
             plugin("versioning", "net.nemerosa.versioning").version("3.0.0")
         }
